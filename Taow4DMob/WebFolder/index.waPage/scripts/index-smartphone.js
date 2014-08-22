60a1c1cbe8a9761ed0e4ff5e4de4a0f9d75ec8d5
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-08-22 10:27:04 +0100

    Initial commit

---
+++ Taow4DMob/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var container4 = {};	// @container
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	container4.click = function container4_click (event)// @startlock
+	{// @endlock
+		$$('tabView1').selectTab(2);
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("container4", "click", container4.click, "WAF");
+// @endregion
+};// @endlock
